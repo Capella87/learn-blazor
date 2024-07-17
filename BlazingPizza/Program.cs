4d9f568745370484ee0f924992f9aa6f4d861ec4
Capella87 <capella87@outlook.com>
2024-07-18 00:08:31 +0900

    [BlazingPizza] Change database to SQLite

--- BlazingPizza/Program.cs
+++ BlazingPizza/Program.cs
@@ -11,11 +11,20 @@ if (builder.Environment.IsDevelopment())
 {
     builder.Configuration.AddUserSecrets<Program>();
 }
-var connection = builder.Configuration.GetConnectionString("SqlServerConnectionString");
-builder.Services.AddDbContext<PizzaStoreContext>(options => options.UseSqlServer(connection));
 builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source = pizza.db");
 
 var app = builder.Build();
+
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 if (!app.Environment.IsDevelopment())
 {
@@ -33,15 +42,5 @@ app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 app.MapControllerRoute("default", "{controller=Home}/{actions=Index}/{id?}");
-
-var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
-using (var scope = scopeFactory.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
-    if (db.Database.EnsureCreated())
-    {
-        SeedData.Initialize(db);
-    }
-}
 
 app.Run();
